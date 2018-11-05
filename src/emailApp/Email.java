619df2bb8e1341b02a9bf6985d4086088fb13c58
ciaranmckenna <hello@ciaranmckenna.com>
2018-11-05 21:35:32 +0000

    Adding the instance variables

--- src/emailApp/Email.java
+++ src/emailApp/Email.java
@@ -2,4 +2,10 @@ package emailApp;
 
 public class Email {
 
+	private String firstName;
+	private String lastName;
+	private String deptName;
+	private String password;
+	private int mailboxCapacity;
+	
 }
